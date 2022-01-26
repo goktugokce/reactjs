2955692f7bafbc9a151354e11f264d44bd371cb1
goktugokce <goktugokce@windowslive.com>
2022-01-26 21:27:16 +0300

    two-way-binding

--- prevstate/src/App.js
+++ prevstate/src/App.js
@@ -1,66 +1,38 @@
 import React, {useState} from 'react';
 
 const App = () => {
-  
-  const [items, setItems] = useState([{
-    id:'1',
-    name:'item1'
-  },
-  {
-    id:'2',
-    name:'item2'
-  },
-  {
-    id:'3',
-    name:'item3'
-  },])
 
   const [inputValue, setInput] = useState({
     inputText:'',
-    inputLength: 0
   });
 
   const inputHandler = (event) => {
+    setInput({
+      ...inputValue,
+      inputText : event.target.value
+    })
     /*setInput((prevState) => {
       return{
       ...prevState,
       inputText: event.target.value}
     });
     */
-      setInput({
-        ...inputValue,
-        inputText : event.target.value
-      })
     }
 
   const submitHandler = (event) => {
     event.preventDefault();
-    setInput((prevState) => {
-      return{
-      ...prevState,
-      inputLength: ((event.target.value).toString())}
-    })
-    console.log(inputValue.inputLength)
-    
-  }
+    setInput({
+      ...inputValue,
+      inputText : ''})
+    console.log(inputValue);
+}
 
 
   return (
     <div>
     <form onSubmit={submitHandler}>
-    <div>
-      <ul>
-        {items.map((el) => {
-          return <li>{el.name}</li>
-        })}
-      </ul>
-    </div>
-    <div>
-      <input onChange={inputHandler} type="text"/>
-    </div>
-    <div>
+      <input onChange={inputHandler} value={inputValue.inputText} type="text"/>
       <button type="submit">Submit</button>
-    </div>
   </form>
   </div>
   )
